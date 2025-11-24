f287c3330cde8655e3aaf95881fd6378c370c2da
Shayan <shayan.haghighi04@gmail.com>
2025-11-24 14:27:49 +0000

    fix fzf init and zsh history

--- zsh/.config/zsh/editing.zsh
+++ zsh/.config/zsh/editing.zsh
@@ -9,6 +9,8 @@ if [[ -z "$_zsh_syntax_highlighting_loaded" ]]; then
     export _zsh_syntax_highlighting_loaded=1
 fi
 
+echo  "export TERM=xterm-256color" >> ~/.zshrc
+
 # zsh-syntax-highlighting styles
 export ZSH_HIGHLIGHT_STYLES=(
   default                          'fg=#CDD6F4'
@@ -41,7 +43,7 @@ fpath=(path/to/zsh-completions/src $fpath)
 eval "$(zoxide init zsh)"
 
 # fzf keybindings
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+#[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # Carapace completion
 #zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
@@ -63,3 +65,7 @@ if [ -x /usr/bin/dircolors ]; then
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
 fi
+setxkbmap -option caps:swapescape
+
+source /usr/share/doc/fzf/examples/key-bindings.zsh
+source /usr/share/doc/fzf/examples/completion.zsh
